2cbd143af4ef623eeddeee7dd03697e25d96ba81
Ale <alearanibar@gmail.com>
2022-04-05 21:32:24 -0700

    test run for color changes

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,57 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, fireEvent } from "@testing-library/react";
+import App from "./App";
+import userEvent from "@testing-library/user-event";
+// test("renders learn react link", () => {
+//   render(<App />);
+//   const linkElement = screen.getByText("Learn React");
+//   expect(linkElement).toBeInTheDocument();
+// });
 
-test('renders learn react link', () => {
+// test("has a word edit", () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/Edit/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+// test("has a word edit", () => {
+//   render(<App />);
+//   const linkElement = screen.getByRole("link", { name: /react/i });
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+// FUNCTIONAL TESTING -----------------------------------
+test("button has correct initial color", () => {
+  // render the component')
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  //  find an element with a role of button and text of 'Change color to blue
+  const colorButton = screen.getByRole("button", { name: /change to blue/i });
+
+  expect(colorButton).toHaveStyle({ backgroundColor: "red" });
 });
+
+test("button turns blue when clicked", () => {
+  // render the component')
+  render(<App />);
+  const colorButton = screen.getByRole("button", { name: "Change to blue" });
+
+  expect(colorButton).toHaveStyle({ backgroundColor: "red" });
+
+  fireEvent.click(colorButton);
+
+  //
+  expect(colorButton).toHaveStyle({ backgroundColor: "blue" });
+  // expect(colorButton).toHaveTextContent("Change to red");
+  // or
+  expect(colorButton.textContent).toBe("Change to red");
+  //
+});
+
+// test("button turns blue when clicked", async () => {
+//   // render the component')
+//   const eve = userEvent.setup();
+//   render(<App />);
+//   const colorButton = await eve.click(
+//     screen.getByRole("button", { name: "Change to blue" })
+//   );
+// });
--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import * as ReactDOMClient from "react-dom/client";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const container = document.getElementById("root");
+
+// Create a root.
+const root = ReactDOMClient.createRoot(container);
+
+// Initial render: Render an element to the root.
+root.render(<App />);
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById("root")
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
